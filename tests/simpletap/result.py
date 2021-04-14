90d9f27a7709cb32e5890076b44b9298d8510985
Renato Alves <alves.rjc@gmail.com>
2021-04-14 19:24:07 +0200

    TST Remove code present in stdlib

--- tests/simpletap/result.py
+++ tests/simpletap/result.py
@@ -12,23 +12,6 @@ import traceback
 import inspect
 
 
-class _WritelnDecorator(object):
-    """Used to decorate file-like objects with a handy 'writeln' method"""
-    def __init__(self, stream):
-        self.stream = stream
-
-    def __getattr__(self, attr):
-        if attr in ('stream', '__getstate__'):
-            raise AttributeError(attr)
-
-        return getattr(self.stream, attr)
-
-    def writeln(self, arg=None):
-        if arg:
-            self.write(arg)
-        self.write('\n')  # text-mode streams translate to \r\n if needed
-
-
 def _color(text, c):
     """
     Add color on the keyword that identifies the state of the test
@@ -49,7 +32,7 @@ def _color(text, c):
 class TAPTestResult(unittest.result.TestResult):
     def __init__(self, stream=sys.stderr, descriptions=True, verbosity=1):
         super(TAPTestResult, self).__init__(stream, descriptions, verbosity)
-        self.stream = _WritelnDecorator(stream)
+        self.stream = unittest.runner._WritelnDecorator(stream)
         self.descriptions = descriptions
         self.verbosity = verbosity
         # Buffer stdout and stderr
